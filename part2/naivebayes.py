77a438988fb2b95bb5a29fe8f9ba2ccd8c625048
Johny Rufus <johnjoh@iu.edu>
2017-11-03 12:24:38 -0700

    Part2: Naive Bayes implementation for OCR

---
+++ part2/naivebayes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import copy
+import math
+
+'''
+A simple Naive Bayes model to predict OCR characters
+'''
+class NaiveBayes:
+    def __init__(self, train_data):
+        self.train_data = train_data
+        # Assume uniform priors for now, can be changed to reflect actual priors based on some general distribution
+        self.prior = {'A':1, 'B':1, 'C':1, 'D':1, 'E':1, 'F':1, 'G':1, 'H':1, 'I':1, 'J':1, 'K':1, 'L':1, 'M':1, 'N':1, 'O':1, 'P':1, 'Q':1, 'R':1, 'S':1, 'T':1, 'U':1, 'V':1, 'W':1, 'X':1, 'Y':1, 'Z':1,
+                      'a':1, 'b':1, 'c':1, 'd':1, 'e':1, 'f':1, 'g':1, 'h':1, 'i':1, 'j':1, 'k':1, 'l':1, 'm':1, 'n':1, 'o':1, 'p':1, 'q':1, 'r':1, 's':1, 't':1, 'u':1, 'v':1, 'w':1, 'x':1, 'y':1, 'z':1,
+                      '0':1, '1':1, '2':1, '3':1, '4':1, '5':1, '6':1, '7':1, '8':1, '9':1,
+                      '(':1, ')':1, ',':1, '.':1, '-':1, '!':1, '?':1, '"':1, '\'':1, ' ':1}
+
+    '''
+    Predicts the values based on training data, and binary features based on pixels, 
+    0 and 1 values are laplace smoothed and transformed to 1/3 and 2/3 respectively.
+    '''
+    def predict(self, test_data):
+        posterior = copy.deepcopy(self.prior)
+        for label in posterior.keys():
+            res = math.log(self.prior[label])
+            for i,row in enumerate(test_data):
+                for j,val in enumerate(row):
+                    res += math.log(2/3) if self.train_data[label][i][j] == test_data[i][j] else math.log(1/3)
+            posterior[label] = res
+        return max(posterior, key=posterior.get)
+
+
--- part2/ocr.py
+++ part2/ocr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # ./ocr.py : Perform optical character recognition, usage:
 #     ./ocr.py train-image-file.png train-text.txt test-image-file.png
@@ -8,6 +8,7 @@
 #
 
 from PIL import Image, ImageDraw, ImageFont
+from naivebayes import NaiveBayes
 import sys
 
 CHARACTER_WIDTH=14
@@ -18,8 +19,8 @@ def load_letters(fname):
     im = Image.open(fname)
     px = im.load()
     (x_size, y_size) = im.size
-    print im.size
-    print int(x_size / CHARACTER_WIDTH) * CHARACTER_WIDTH
+    #print(im.size)
+    #print(int(x_size / CHARACTER_WIDTH) * CHARACTER_WIDTH)
     result = []
     for x_beg in range(0, int(x_size / CHARACTER_WIDTH) * CHARACTER_WIDTH, CHARACTER_WIDTH):
         result += [ [ "".join([ '*' if px[x, y] < 1 else ' ' for x in range(x_beg, x_beg+CHARACTER_WIDTH) ]) for y in range(0, CHARACTER_HEIGHT) ], ]
@@ -32,9 +33,20 @@ def load_training_letters(fname):
 
 #####
 # main program
-(train_img_fname, train_txt_fname, test_img_fname) = sys.argv[1:]
+
+train_img_fname = 'courier-train.png'
+test_img_fname = 'test-0-0.png'
 train_letters = load_training_letters(train_img_fname)
 test_letters = load_letters(test_img_fname)
+nb = NaiveBayes(train_letters)
+for letter in test_letters:
+    print(nb.predict(letter))
+
+
+
+#(train_img_fname, train_txt_fname, test_img_fname) = sys.argv[1:]
+
+
 
 ## Below is just some sample code to show you how the functions above work. 
 # You can delete them and put your own code here!
@@ -43,11 +55,11 @@ test_letters = load_letters(test_img_fname)
 # Each training letter is now stored as a list of characters, where black
 #  dots are represented by *'s and white dots are spaces. For example,
 #  here's what "a" looks like:
-print "\n".join([ r for r in train_letters['a'] ])
+#print("\n".join([ r for r in train_letters['a'] ]))
 
 # Same with test letters. Here's what the third letter of the test data
 #  looks like:
-print "\n".join([ r for r in test_letters[2] ])
+#print("\n".join([ r for r in test_letters[2] ]))
 
 
 

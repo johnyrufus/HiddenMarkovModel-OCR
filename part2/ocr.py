91c08dc1e279740bceb9036e6e33885a7843e746
Johny Rufus <johnjoh@iu.edu>
2017-11-04 11:25:16 -0700

    Part2: Simplified: print result in format

--- part2/ocr.py
+++ part2/ocr.py
@@ -19,47 +19,53 @@ def load_letters(fname):
     im = Image.open(fname)
     px = im.load()
     (x_size, y_size) = im.size
-    #print(im.size)
-    #print(int(x_size / CHARACTER_WIDTH) * CHARACTER_WIDTH)
     result = []
     for x_beg in range(0, int(x_size / CHARACTER_WIDTH) * CHARACTER_WIDTH, CHARACTER_WIDTH):
         result += [ [ "".join([ '*' if px[x, y] < 1 else ' ' for x in range(x_beg, x_beg+CHARACTER_WIDTH) ]) for y in range(0, CHARACTER_HEIGHT) ], ]
     return result
+
 
 def load_training_letters(fname):
     TRAIN_LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789(),.-!?\"' "
     letter_images = load_letters(fname)
     return { TRAIN_LETTERS[i]: letter_images[i] for i in range(0, len(TRAIN_LETTERS) ) }
 
-#####
-# main program
 
-train_img_fname = 'courier-train.png'
-test_img_fname = 'test-0-0.png'
-train_letters = load_training_letters(train_img_fname)
-test_letters = load_letters(test_img_fname)
-nb = NaiveBayes(train_letters)
-for letter in test_letters:
-    print(nb.predict(letter))
+def simplified_bayes(train_letters, test_letters):
+    nb = NaiveBayes(train_letters)
+    return ''.join([nb.predict(letter) for letter in test_letters])
 
 
-
-#(train_img_fname, train_txt_fname, test_img_fname) = sys.argv[1:]
-
-
-
-## Below is just some sample code to show you how the functions above work. 
-# You can delete them and put your own code here!
+def hmm_ve(train_letters, test_letters):
+    pass
 
 
-# Each training letter is now stored as a list of characters, where black
-#  dots are represented by *'s and white dots are spaces. For example,
-#  here's what "a" looks like:
-#print("\n".join([ r for r in train_letters['a'] ]))
+def hmm_map(train_letters, test_letters):
+    pass
 
-# Same with test letters. Here's what the third letter of the test data
-#  looks like:
-#print("\n".join([ r for r in test_letters[2] ]))
+
+def main():
+    train_img_fname = 'courier-train.png'
+    test_img_fname = 'test-0-0.png'
+
+    train_letters = load_training_letters(train_img_fname)
+    test_letters = load_letters(test_img_fname)
+
+    # Simplified
+    print('Simple: {}'.format(simplified_bayes(train_letters, test_letters)))
+
+    # HMM VE
+    print('HMM VE: {}'.format(hmm_ve(train_letters, test_letters)))
+
+    # HMM MAP
+    print('HMM MAP: {}'.format(hmm_map(train_letters, test_letters)))
+
+
+if __name__ == '__main__':
+    main()
+
+
+
 
 
 

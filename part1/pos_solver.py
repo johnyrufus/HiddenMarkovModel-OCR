ff228fc67d6748cbb2a011eb347406cdec0ce2ae
anduer <anduer@iu.edu>
2017-11-04 21:53:40 -0700

    Add Viterbi Decoding
    Created and added in the final algorithm - Viterbi decoding. Model shows 94.34% word accuracy and 50.7% sentence accuracy, which beats simplified (93.95%/47.5%) and HMM-VE (92.37%/40.4%). Won't claim it can't run faster or that it's 100% bug free, but I think it's pretty close. :)

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -12,6 +12,8 @@
 
 import random
 import math
+import numpy as np
+from operator import itemgetter
 from copy import deepcopy
 
 order = ["adj", "adv", "adp", "conj", "det", "noun", "num", "pron", "prt", \
@@ -30,6 +32,11 @@ class Solver:
         #Class variables for the HMM-VE model.
         self.initial_state = [0.00] * 12
         self.transitions = [[0.00] * 12 for x in range(12)]
+
+        #Class variables for the HMM-Viterbi model.
+        self.emissions_log = {}
+        self.initial_state_log = [0.00] * 12
+        self.transitions_log = [[0.00] * 12 for x in range(12)]
         
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
@@ -40,12 +47,14 @@ class Solver:
     #
     def train(self, data):
         #Simplified Dictionary Creation
+        start_of_sentence = [0.00] * 12
+        
         for example in data:           
             sentence = example[0]
             tags = example[1]
             
             index_of_tag = order.index(tags[0])
-            self.initial_state[index_of_tag] += 1
+            start_of_sentence[index_of_tag] += 1
         
             last_tag = None
             for word, tag in zip(sentence, tags):
@@ -62,22 +71,29 @@ class Solver:
                 last_tag = tag
         
         #Convert initial states to percentages
-        total = sum(self.initial_state)
-        self.initial_state = [1.00 * x / total for x in self.initial_state]
+        total = sum(start_of_sentence)
+        self.initial_state = [1.00 * x / total for x in start_of_sentence]
+        self.initial_state_log = [math.log(1.00 * (x + 0.0001) / (total + 0.0012), 2) for x in start_of_sentence]        
  
         #Convert emission probabilities over to percentages.
         for key in self.emissions:
-            values = self.emissions[key]
-            total = sum(values)
+            totals = self.emissions[key]
+            total = sum(totals)
             
-            values = [1.00 * x / total for x in values]
-            self.emissions[key] = values     
+            emission_prob = [1.00 * x / total for x in totals]
+            self.emissions[key] = emission_prob
+
+            emission_prob_log = [math.log(1.00 * (x + 0.0001) / (total + 0.0012), 2) for x in totals]
+            self.emissions_log[key] = emission_prob_log
         
         #Convert transitions over to percentages...
         for i, row in enumerate(self.transitions):
             total = sum(row)
             row = [1.00 * x / total for x in row]
             self.transitions[i] = row
+            
+            row_log = [math.log(1.00 * (x + 0.0001) / (total + 0.0012), 2) for x in row]
+            self.transitions_log[i] = row_log
         pass
     
     # Functions for each algorithm.
@@ -117,9 +133,8 @@ class Solver:
                 for n, new in enumerate(new_state):
                     for p, pos in enumerate(state):
                         trans_prob = self.transitions[p][n]
-                        prev_prob = state[p]
                         
-                        new_state[n] += trans_prob * prev_prob
+                        new_state[n] += trans_prob * pos
                     
                     #If we've never encountered this word before, then
                     #let everything have equal probability of emission.
@@ -136,8 +151,63 @@ class Solver:
         return guess
 
     def hmm_viterbi(self, sentence):
-        return [ "noun" ] * len(sentence)
+        guess = []
+        
+        state = [[0.00] * 12 for x in range(len(sentence))]
+        backtrack = []
+        
+        for w, word in enumerate(sentence):
+            if word in self.emissions_log:
+                em = self.emissions_log[word]
+            else:
+                em = [0.00] * 12
+            
+            if w == 0:
+                cur_state = state[w]
+                
+                for c, c_pos in enumerate(cur_state):
+                    value = self.initial_state_log[c] + em[c]
+                    cur_state[c] = value
+                state[w] = cur_state
+            else:
+                prev_state = state[w-1]
+                cur_state = state[w]
+                
+                for c, c_pos in enumerate(cur_state):
+                    prev_val = -float('inf')
+                    for p, p_pos in enumerate(prev_state):
+                        prev_val = max(prev_val, p_pos + self.transitions_log[p][c])
+                    
+                    value = em[c] + prev_val
+                    cur_state[c] = value
+                state[w] = cur_state
 
+                back = [[0.00] * 12 for x in range(12)]
+                for c, c_pos in enumerate(cur_state):
+                    for p, p_pos in enumerate(prev_state):
+                        back[p][c] = p_pos + self.transitions_log[p][c] + em[c]
+                backtrack.append(back)
+
+        last = state[len(sentence)-1]
+        last_index = last.index(max(last))
+        tagging = [last_index]
+
+        for trans in reversed(backtrack):
+            high_prob = -float('inf')
+            index = -1
+            
+            for p, pos in enumerate(trans):
+                if pos[last_index] > high_prob:
+                    high_prob = pos[last_index]
+                    index = p
+            
+            last_index = index
+            tagging = [last_index] + tagging
+
+        for tag in tagging:
+            guess += [order[tag]]
+
+        return guess
 
     # This solve() method is called by label.py, so you should keep the interface the
     #  same, but you can change the code itself. 

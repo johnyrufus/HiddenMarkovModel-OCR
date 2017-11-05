7fcc2d76d52435c5b3504502bfe76b1141749c5b
anduer <anduer@iu.edu>
2017-11-05 15:06:12 -0800

    Reworked HMM-VE to use correct Probabilities
    Reworked the variables and flipped around the HMM-VE model to use the correct probability of P(W|POS). Right now simple shows 93.95%/47.50% while HMM-VE shows 87.55%/30.85%. Not sure why it's decreasing but I think it has to do with the Laplace smoothing (self.ve_default) being too high.

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -10,11 +10,7 @@
 # Put your report here!!
 ####
 
-import random
 import math
-import numpy as np
-from operator import itemgetter
-from copy import deepcopy
 
 order = ["adj", "adv", "adp", "conj", "det", "noun", "num", "pron", "prt", \
          "verb", "x", "."]
@@ -26,17 +22,21 @@ order = ["adj", "adv", "adp", "conj", "det", "noun", "num", "pron", "prt", \
 class Solver:
     def __init__(self):
         #Class variables for the simplified POS model.
-        self.emissions = {}
-        self.totals = [0.00] * 12
-        
-        #Class variables for the HMM-VE model.
-        self.initial_state = [0.00] * 12
-        self.transitions = [[0.00] * 12 for x in range(12)]
+        self.simple_p_of_pos_given_word = {}
+        self.simple_p_of_pos = [0.00] * 12  
 
-        #Class variables for the HMM-Viterbi model.
-        self.emissions_log = {}
-        self.initial_state_log = [0.00] * 12
-        self.transitions_log = [[0.00] * 12 for x in range(12)]
+        #Class variables for the HMM-VE model.
+        self.ve_emissions = []
+        self.ve_initial_state = [0.00] * 12
+        self.ve_transitions = [[0.00] * 12 for _ in range(12)]
+        for _ in range(12): self.ve_emissions.append({})
+        self.ve_default = [0.00] * 12
+
+#        #Class variables for the HMM-Viterbi model.
+#        self.emissions_log = {}
+#        self.initial_state_log = [0.00] * 12
+#        self.transitions_log = [[0.00] * 12 for x in range(12)]
+#        self.default_log = [0.00] * 12
         
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
@@ -46,71 +46,89 @@ class Solver:
     # Do the training!
     #
     def train(self, data):
-        #Simplified Dictionary Creation
-        start_of_sentence = [0.00] * 12
+        #Count how many times a tag starts a sentence
         
-        for example in data:           
+        for example in data:
             sentence = example[0]
             tags = example[1]
+
+            #Variables for VE algorithm.
+            #Count the times a tag starts a sentence.
+            self.ve_initial_state[order.index(tags[0])] += 1
             
-            index_of_tag = order.index(tags[0])
-            start_of_sentence[index_of_tag] += 1
-        
             last_tag = None
             for word, tag in zip(sentence, tags):
-                if word not in self.emissions:
-                    self.emissions[word] = [0.00] * 12
-            
                 index_of_tag = order.index(tag)
-                self.emissions[word][index_of_tag] += 1
-                self.totals[index_of_tag] += 1
                 
+                #Variables for simplified algorithm.
+                self.simple_p_of_pos[index_of_tag] += 1
+                if word not in self.simple_p_of_pos_given_word:
+                    self.simple_p_of_pos_given_word[word] = [0.00] * 12
+                self.simple_p_of_pos_given_word[word][index_of_tag] += 1
+        
+                #Recording Transition Probabilities
                 if last_tag != None:
-                    index_of_last = order.index(last_tag)
-                    self.transitions[index_of_last][index_of_tag] += 1
-                last_tag = tag
-        
-        #Convert initial states to percentages
-        total = sum(start_of_sentence)
-        self.initial_state = [1.00 * x / total for x in start_of_sentence]
-        self.initial_state_log = [math.log(1.00 * (x + 0.0001) / (total + 0.0012), 2) for x in start_of_sentence]        
- 
-        #Convert emission probabilities over to percentages.
-        for key in self.emissions:
-            totals = self.emissions[key]
-            total = sum(totals)
-            
-            emission_prob = [1.00 * x / total for x in totals]
-            self.emissions[key] = emission_prob
+                    last_tag_index = order.index(last_tag)
+                    current_tag_index = order.index(tag)
+                    
+                    self.ve_transitions[last_tag_index][current_tag_index] += 1
 
-            emission_prob_log = [math.log(1.00 * (x + 0.0001) / (total + 0.0012), 2) for x in totals]
-            self.emissions_log[key] = emission_prob_log
+                #Recording Emission Probabilities
+                if word not in self.ve_emissions[index_of_tag]:
+                    self.ve_emissions[index_of_tag][word] = 0
+                self.ve_emissions[index_of_tag][word] += 1
+                
+                last_tag = tag
+                
+        #Converting P(pos) to percentages
+        total = sum(self.simple_p_of_pos)
+        for p, pos in enumerate(self.simple_p_of_pos):
+            self.simple_p_of_pos[p] = pos / total
         
-        #Convert transitions over to percentages...
-        for i, row in enumerate(self.transitions):
-            total = sum(row)
-            row = [1.00 * x / total for x in row]
-            self.transitions[i] = row
+        #Converting P(pos|word) to percentages
+        for word in self.simple_p_of_pos_given_word:
+            probs = self.simple_p_of_pos_given_word[word]
             
-            row_log = [math.log(1.00 * (x + 0.0001) / (total + 0.0012), 2) for x in row]
-            self.transitions_log[i] = row_log
+            total = sum(probs)
+            probs = [x / total for x in probs]
+            
+            self.simple_p_of_pos_given_word[word] = probs
+
+        #Converting initial states to percentages
+        total = sum(self.ve_initial_state)
+        self.ve_initial_state = [x / total for x in self.ve_initial_state]
+
+        #Converting transitions over to percentages:
+        for p, pos in enumerate(self.ve_transitions):
+            total = sum(pos)
+            probs = [x / total for x in pos]
+            self.ve_transitions[p] = probs
+
+        #Converting emission probabilities over to percentages:
+        for p, pos in enumerate(self.ve_emissions):
+            total = sum(pos.values())
+            
+            for word in pos:
+                pos[word] = 1.00 * pos[word] / total
+            
+            #Record our probability of never having encountered a word.
+            self.ve_default[p] = 1.00 / total
         pass
     
     # Functions for each algorithm.
     #
     def simplified(self, sentence):
-        guess = []
-        
+        guess = []        
         for word in sentence:
-            if word in self.emissions:
-                values = self.emissions[word]
-                part = values.index(max(values))
+            if word in self.simple_p_of_pos_given_word:
+                probs = self.simple_p_of_pos_given_word[word]
+                part = probs.index(max(probs))
                 
                 guess += [order[part]]
             else:
                 #If we've never encountered the word before, pick the most
                 #common part-of-specch.
-                part = self.totals.index(max(self.totals))
+                part = self.simple_p_of_pos.index(max(self.simple_p_of_pos))
                 guess += [order[part]]
         return guess
 
@@ -118,94 +136,94 @@ class Solver:
         guess = []
         
         state = [0.00] * 12
-        for i, word in enumerate(sentence):            
-            if i == 0:
-                for y, pos in enumerate(state):
-                    pos = self.initial_state[y]
-
-                    #If we've never encountered this word before, then
-                    #let everything have equal probability of emission.
-                    if word in self.emissions:
-                        pos *= self.emissions[word][y]
-                    state[y] = pos
+        for w, word in enumerate(sentence):
+            if w == 0:
+                for p, pos in enumerate(state):
+                    prob = self.ve_initial_state[p]
+                    
+                    if word in self.ve_emissions[p]:
+                        prob *= self.ve_emissions[p][word]
+                    else:
+                        prob *= self.ve_default[p]
+                    
+                    state[p] = prob
             else:
                 new_state = [0.00] * 12
-                for n, new in enumerate(new_state):
-                    for p, pos in enumerate(state):
-                        trans_prob = self.transitions[p][n]
-                        
-                        new_state[n] += trans_prob * pos
-                    
+                for new_p, new_pos in enumerate(new_state):
+                    for old_p, old_pos in enumerate(state):
+                        trans_prob = self.ve_transitions[old_p][new_p]                        
+                        new_state[new_p] += trans_prob * old_pos
                     #If we've never encountered this word before, then
                     #let everything have equal probability of emission.
-                    if word in self.emissions:
-                        new_state[n] *= self.emissions[word][n]
+                    if word in self.ve_emissions[new_p]:
+                        new_state[new_p] *= self.ve_emissions[new_p][word]
+                    else:
+                        new_state[new_p] *= self.ve_default[new_p]
                 state = new_state
             
             total = sum(state)
             state = [1.00 * x / total for x in state]
-            
+
             part = state.index(max(state))
-            guess += [order[part]]
-        
+            guess += [order[part]]   
         return guess
 
     def hmm_viterbi(self, sentence):
-        guess = []
-        
-        state = [[0.00] * 12 for x in range(len(sentence))]
-        backtrack = []
-        
-        for w, word in enumerate(sentence):
-            if word in self.emissions_log:
-                em = self.emissions_log[word]
-            else:
-                em = [0.00] * 12
-            
-            if w == 0:
-                cur_state = state[w]
-                
-                for c, c_pos in enumerate(cur_state):
-                    value = self.initial_state_log[c] + em[c]
-                    cur_state[c] = value
-                state[w] = cur_state
-            else:
-                prev_state = state[w-1]
-                cur_state = state[w]
-                
-                for c, c_pos in enumerate(cur_state):
-                    prev_val = -float('inf')
-                    for p, p_pos in enumerate(prev_state):
-                        prev_val = max(prev_val, p_pos + self.transitions_log[p][c])
-                    
-                    value = em[c] + prev_val
-                    cur_state[c] = value
-                state[w] = cur_state
-
-                back = [[0.00] * 12 for x in range(12)]
-                for c, c_pos in enumerate(cur_state):
-                    for p, p_pos in enumerate(prev_state):
-                        back[p][c] = p_pos + self.transitions_log[p][c] + em[c]
-                backtrack.append(back)
-
-        last = state[len(sentence)-1]
-        last_index = last.index(max(last))
-        tagging = [last_index]
-
-        for trans in reversed(backtrack):
-            high_prob = -float('inf')
-            index = -1
-            
-            for p, pos in enumerate(trans):
-                if pos[last_index] > high_prob:
-                    high_prob = pos[last_index]
-                    index = p
-            
-            last_index = index
-            tagging = [last_index] + tagging
-
-        for tag in tagging:
-            guess += [order[tag]]
+        guess = ["noun"] * len(sentence)
+#        
+#        state = [[0.00] * 12 for x in range(len(sentence))]
+#        backtrack = []
+#        
+#        for w, word in enumerate(sentence):
+#            if word in self.emissions_log:
+#                em = self.emissions_log[word]
+#            else:
+#                em = self.default_log
+#            
+#            if w == 0:
+#                cur_state = state[w]
+#                
+#                for c, c_pos in enumerate(cur_state):
+#                    value = self.initial_state_log[c] + em[c]
+#                    cur_state[c] = value
+#                state[w] = cur_state
+#            else:
+#                prev_state = state[w-1]
+#                cur_state = state[w]
+#                
+#                for c, c_pos in enumerate(cur_state):
+#                    prev_val = -float('inf')
+#                    for p, p_pos in enumerate(prev_state):
+#                        prev_val = max(prev_val, p_pos + self.transitions_log[p][c])
+#                    
+#                    value = em[c] + prev_val
+#                    cur_state[c] = value
+#                state[w] = cur_state
+#
+#                back = [[0.00] * 12 for x in range(12)]
+#                for c, c_pos in enumerate(cur_state):
+#                    for p, p_pos in enumerate(prev_state):
+#                        back[p][c] = p_pos + self.transitions_log[p][c] + em[c]
+#                backtrack.append(back)
+#
+#        last = state[len(sentence)-1]
+#        last_index = last.index(max(last))
+#        tagging = [last_index]
+#
+#        for trans in reversed(backtrack):
+#            high_prob = -float('inf')
+#            index = -1
+#            
+#            for p, pos in enumerate(trans):
+#                if pos[last_index] > high_prob:
+#                    high_prob = pos[last_index]
+#                    index = p
+#            
+#            last_index = index
+#            tagging = [last_index] + tagging
+#
+#        for tag in tagging:
+#            guess += [order[tag]]
 
         return guess
 
@@ -223,4 +241,3 @@ class Solver:
             return self.hmm_viterbi(sentence)
         else:
             print "Unknown algo!"
-

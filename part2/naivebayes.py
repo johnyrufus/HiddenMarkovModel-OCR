840537d059e3fc0d104359444c2821cdf7f86670
Johny Rufus <johnjoh@iu.edu>
2017-11-12 05:55:12 -0800

    Part2: Naive Bayes: Change to include pixel count in the way characters are handled

--- part2/naivebayes.py
+++ part2/naivebayes.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+#
+# ./ocr.py : Perform optical character recognition, usage:
+#     ./ocr.py train-image-file.png train-text.txt test-image-file.png
+#
+# Authors: Johny Rufus
+# (based on skeleton code by D. Crandall, Oct 2017)
+#
 
 import copy
 import math
@@ -17,17 +24,32 @@ class NaiveBayes:
                       '(':1, ')':1, ',':1, '.':1, '-':1, '!':1, '?':1, '"':1, '\'':1, ' ':1}
         self.prior = prior
 
+        self.pixel_count = {}
+        for ch in prior.keys():
+            total_on = 0
+            for pix in train_data[ch]:
+                total_on += 1 if pix == '*' else 0
+            self.pixel_count[ch] = total_on
+        self.avg_pixel_count = sum(self.pixel_count.values()) / len(train_data)
+
     '''
     Predicts the values based on training data, and binary features based on pixels, 
     0 and 1 values are laplace smoothed and transformed to 1/3 and 2/3 respectively.
     '''
     def predict(self, test_data):
         posterior = copy.deepcopy(self.prior)
+        total_on = 0
+        for pix in test_data:
+            total_on += 1 if pix == '*' else 0
         for label in posterior.keys():
             res = math.log(self.prior[label])
             for i, val in enumerate(test_data):
-                if test_data[i] == '*':
-                    res += math.log(2 / 3) if self.train_data[label][i] == test_data[i] else math.log(1 / 3)
+                if total_on < self.avg_pixel_count/5:
+                    if test_data[i] == ' ':
+                        res += math.log(2/3) if self.train_data[label][i] == test_data[i] else math.log(1/3)
+                else:
+                    if test_data[i] == '*':
+                        res += math.log(2/3) if self.train_data[label][i] == test_data[i] else math.log(1/3)
             posterior[label] = res
         return max(posterior, key=posterior.get)
 
